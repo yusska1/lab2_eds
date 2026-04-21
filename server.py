0578f65a6c9a9250031d799185ae26ddd14404d9
yusska1 <yuskaskh@gmail.com>
2026-04-21 13:15:45 +0300

    lab2_eds

--- server.py
+++ server.py
@@ -6,31 +6,32 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 app = Flask(__name__)
 
 server_private_key = rsa.generate_private_key(
-    public_exponent=65537,  
-    key_size=2048          
+    public_exponent=65537,
+    key_size=2048
+)
 
 server_public_key = server_private_key.public_key()
 
-# Сценарий 1 - Проверка подписи клиента
+
 @app.route("/verify", methods=["POST"])
 def verify():
 
-       data = request.json
+    data = request.json
 
     message = data["message"].encode()
 
     signature = bytes.fromhex(data["signature"])
 
-        client_public_key = serialization.load_pem_public_key(
+    client_public_key = serialization.load_pem_public_key(
         data["public_key"].encode()
     )
 
     try:
-            client_public_key.verify(
-            signature,           
-            message,             
-            padding.PKCS1v15(),  
-            hashes.SHA256()      
+        client_public_key.verify(
+            signature,
+            message,
+            padding.PKCS1v15(),
+            hashes.SHA256()
         )
 
         return jsonify({"status": "valid"})
@@ -39,27 +40,27 @@ def verify():
         return jsonify({"status": "invalid"})
 
 
-# Сценарий 2 - Сервер подписывает сообщение
 @app.route("/sign", methods=["GET"])
 def sign():
 
     message = "Hello from server"
 
-        signature = server_private_key.sign(
+    signature = server_private_key.sign(
         message.encode(),
         padding.PKCS1v15(),
         hashes.SHA256()
     )
 
-      return jsonify({
+    return jsonify({
         "message": message,
         "signature": signature.hex()
     })
 
+
 @app.route("/public_key", methods=["GET"])
 def get_public_key():
 
-        public_pem = server_public_key.public_bytes(
+    public_pem = server_public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     ).decode()
